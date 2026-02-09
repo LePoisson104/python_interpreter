89aa713a704059616dd91111b071c943a1d40d7e
Viet Pham <vietpham2017@gmail.com>
2026-02-08 23:04:50 -0600

    progress

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+std::string read_file(const std::string &filename)
+{
+    std::ifstream file(filename);
+    if (!file)
+    {
+        throw std::runtime_error("Failed to open file");
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
 
 // g++ main.cpp -o main && ./main test.py (to run the program)
 int main(int argc, char **argv)
@@ -9,7 +25,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    std::cout << "hello world" << std::endl;
+    std::string source_code = read_file(argv[1]);
+    std::cout << source_code;
 
     return 0;
 }
